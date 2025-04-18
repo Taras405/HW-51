240c538263908f796b5629f23ab3f6d92b7e4dc6
taki <tarassevcuk700@gmail.com>
2025-04-18 17:52:45 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,21 @@
-const result = 5+5+'5';
+const result = 5 + 5 + '5';
+console.log(result);
 console.log(typeof result);
 
 
-const email = '@';
+const email = 'tarassevcuk700@gmail.com';
+console.log(email.includes('@'));
 console.log(email.length);
+console.log(email);
 
 
 const word1 = 'My ';
 const word2 = 'name ';
 const word3 = 'is ';
-
 const fullName = word1 + word2 + word3 + 'Victor';
 console.log(fullName);
 
+
 const userName = 'Олександро';
 const payment = 300;
-
 alert(`Дякуємо, ${userName}! До сплати ${payment} гривень`);
